8c3e652e6dd322bd337891149fcc3f0d46093e5e
ririkku <ryoji19950109@gmail.com>
2020-10-18 10:47:15 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = props => {
 
   const [state, setState] = useState(props);
   const {name, price} = state;
+
+  
+  useEffect(() => {
+    // useEffectをcomponentDidMountのように扱いたい場合は、第二引数に空配列を渡す
+    console.log('This is like componentDidMount');
+  }, []);
+
+  useEffect(() => {
+    // 状態管理しているnameが変更されたタイミングで呼び出したい場合、第二引数にnameを配列に入れて渡す
+    console.log('This callback is for name only');
+  }, [name]);
+
+  // 状態が変わる->レンダリング->useEffectの順に呼び出される模様(ComponentDidMonuntとかdidUpdateっぽい？)
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidMount');
+  });
 
   return (
     <>
