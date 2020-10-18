22306435cd2c586b86963bce4bd5dbf9971a34d0
ririkku <ryoji19950109@gmail.com>
2020-10-18 10:25:19 +0900

    複数状態の管理

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React, {useState} from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [xxx, setXxx] = useState(0);
+const App = props => {
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const twoTimes = () => setCount(previousCount => previousCount * 2);
-
-  const onlyThree = () => {
-    if(count % 3 !== 0) {
-      console.log('割り切れない！');
-      return setXxx(0);
-    }
-    const value = count / 3;
-    return setXxx(value);
+  const reset = () => {
+    setName(props.name);
+    setPrice(props.price);
   }
-
-  const reset = () => setCount(0);
 
   return (
     <>
-      <p>count: {count}</p>
-      <p>3で割った時のあれ。: {xxx}</p>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={twoTimes}>X2</button>
-      <button onClick={onlyThree}>3の倍数の時だけ3で割る</button>
-      <br></br>
-      <button onClick={reset}>reset</button>
+    <p>{name}は, {price}円</p>
+    <button onClick={() => setPrice(price + 1)}>+1</button>
+    <button onClick={() => setPrice(price - 1)}>-1</button>
+    <button onClick={reset}>reset</button>
+    <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: 'サンプル',
+  price: 100
 }
 
 export default App;
