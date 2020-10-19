e6118b44204bbe659a16c7d2315b7fa7fc0e2f09
ririkku <ryoji19950109@gmail.com>
2020-10-20 01:21:04 +0900

    add use context

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
 
-export default CREATE_EVENT = 'CREATE_EVENT';
-export default DELETE_EVENT = 'DELETE_EVENT';
-export default DELETE_ALL_EVENTS = 'DELETE_ALL_EVENTS';
+export const CREATE_EVENT = 'CREATE_EVENT';
+export const DELETE_EVENT = 'DELETE_EVENT';
+export const DELETE_ALL_EVENTS = 'DELETE_ALL_EVENTS';
